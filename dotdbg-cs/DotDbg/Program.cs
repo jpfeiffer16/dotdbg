dcf4d93881b729615515263cc6989e2631e06d52
jpfeiffer16 <joepfeiffer16@gmail.com>
2019-05-05 22:48:20 -0600

    Getting ready for bootstrap to c#

---
+++ dotdbg-cs/DotDbg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotDbg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
